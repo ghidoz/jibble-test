774987dc9df9a83b8a1601f464814b9eb8c3320f
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2017-11-22 19:01:48 +0100

    Add PostService with query method & Core module

---
+++ src/app/core/post/post.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs/Observable';
+import { Post } from './post.interface';
+
+@Injectable()
+export class PostService {
+
+  private apiUrl = environment.baseApiUrl + 'posts';
+
+  constructor(private http: HttpClient) { }
+
+  query(): Observable<Post> {
+    return this.http.get<Post>(this.apiUrl);
+  }
+
+}
