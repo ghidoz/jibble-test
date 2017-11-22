c7d3d8ad24e4cf930e6310b64ad4827be3e33ba3
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2017-11-22 23:53:02 +0100

    Add tests for services

--- src/app/core/post/post.service.spec.ts
+++ src/app/core/post/post.service.spec.ts
@@ -1,15 +1,53 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import 'rxjs/add/observable/of';
 import { PostService } from './post.service';
+import { Post } from './post.interface';
+import { POST_RESPONSE } from '../../../tests/post.fixture';
+
+export class MockHttpClient {
+  get(): Observable<Post[]> {
+    return Observable.of(POST_RESPONSE);
+  }
+}
 
 describe('PostService', () => {
+
+  let service: PostService;
+  let http: HttpClient;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [PostService]
+      providers: [
+        PostService,
+        {provide: HttpClient, useClass: MockHttpClient}
+      ]
+    });
+    service = TestBed.get(PostService);
+    http = TestBed.get(HttpClient);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('query', () => {
+    let response: Post[];
+    beforeEach(() => {
+      spyOn(http, 'get').and.callThrough();
+      service.query().subscribe((resp: Post[]) => {
+        response = resp;
+      });
+    });
+    it('should call http.get', () => {
+      expect(http.get).toHaveBeenCalledWith(environment.baseApiUrl + 'posts');
+    });
+    it('should return response', () => {
+      expect(response).toEqual(POST_RESPONSE);
     });
   });
 
-  it('should be created', inject([PostService], (service: PostService) => {
-    expect(service).toBeTruthy();
-  }));
 });
